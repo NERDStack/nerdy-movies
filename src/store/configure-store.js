13c9e2c6169919a41d46d4b91f1e6f73d6e303c4
Thomas Stringer <tr.stringer@outlook.com>
2016-11-22 16:51:27 -0500

    add redux devtools ext

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -2,6 +2,8 @@ import rootReducer from '../reducers';
 import { createStore } from 'redux';
 
 export default (initialState) => {
-  return createStore(rootReducer, initialState);
+  return createStore(rootReducer, initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 };
 
