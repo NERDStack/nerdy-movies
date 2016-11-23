09b85f1fda9d7de0d02d1b91ca33f51026d534c0
Thomas Stringer <tr.stringer@outlook.com>
2016-11-23 10:17:53 -0500

    add sample initial state and fix list wrap

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/configure-store';
 
-const store = configureStore();
+const store = configureStore({
+  movies: [
+    { name: 'Movie One' },
+    { name: 'Movie Two' },
+    { name: 'Movie Three' },
+    { name: 'Movie Four' },
+    { name: 'Movie Five' },
+    { name: 'Movie Six' },
+    { name: 'Movie Seven' }
+]});
 
 ReactDOM.render(
   <Provider store={store}>
