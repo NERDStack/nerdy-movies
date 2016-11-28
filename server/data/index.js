d13bfb955011f3f35435b85640c7962c40a60e38
Thomas Stringer <tr.stringer@outlook.com>
2016-11-28 11:24:49 -0500

    remove unnecessary namespacing pattern

--- server/data/index.js
+++ server/data/index.js
@@ -1,91 +1,84 @@
 const DocumentDBClient = require('documentdb').DocumentClient;
 const dataConfig = require('./config');
 
-module.exports = (() => {
-  const collLink = `dbs/${dataConfig.databaseName}/colls/${dataConfig.collectionName}`;
+const collLink = `dbs/${dataConfig.databaseName}/colls/${dataConfig.collectionName}`;
 
-  function queryMovies(id) {
-    return new Promise((resolve, reject) => {
-      const docdbClient = new DocumentDBClient(
-        process.env.DOCUMENTDB_URI,
-        { masterKey: process.env.DOCUMENTDB_KEY });
+module.exports.queryMovies = (id) => {
+  return new Promise((resolve, reject) => {
+    const docdbClient = new DocumentDBClient(
+      process.env.DOCUMENTDB_URI,
+      { masterKey: process.env.DOCUMENTDB_KEY });
 
-      if (id) {
-        const querySpec = {
-          query: 'SELECT * FROM root r WHERE r.id = @id',
-          parameters: [{
-            name: '@id',
-            value: id
-          }]
-        };
-        docdbClient.queryDocuments(collLink, querySpec)
-          .toArray((err, docs) => {
-            if (err) {
-              reject(err);
-            }
-            else {
-              resolve(docs);
-            }
-          });
+    if (id) {
+      const querySpec = {
+        query: 'SELECT * FROM root r WHERE r.id = @id',
+        parameters: [{
+          name: '@id',
+          value: id
+        }]
+      };
+      docdbClient.queryDocuments(collLink, querySpec)
+        .toArray((err, docs) => {
+          if (err) {
+            reject(err);
+          }
+          else {
+            resolve(docs);
+          }
+        });
+    }
+    else {
+      docdbClient.readDocuments(collLink)
+        .toArray((err, docs) => {
+          if (err) {
+            reject(err);
+          }
+          else {
+            resolve(docs);
+          }
+        });
+    }
+  });
+}
+
+module.exports.likeMovie = (movie) => {
+  return new Promise((resolve, reject) => {
+    const docdbClient = new DocumentDBClient(
+      process.env.DOCUMENTDB_URI,
+      { masterKey: process.env.DOCUMENTDB_KEY });
+
+    // const documentLink = `${collLink}/${movie.id}`;
+    const documentLink = movie._self;
+    const movieUpdate = Object.assign({}, movie, { liked: true });
+
+    docdbClient.replaceDocument(documentLink, movieUpdate, (err, updated) => {
+      if (err) {
+        reject(err);
       }
       else {
-        docdbClient.readDocuments(collLink)
-          .toArray((err, docs) => {
-            if (err) {
-              reject(err);
-            }
-            else {
-              resolve(docs);
-            }
-          });
+        resolve(updated);
       }
     });
-  }
+  });
+}
 
-  function likeMovie(movie) {
-    return new Promise((resolve, reject) => {
-      const docdbClient = new DocumentDBClient(
-        process.env.DOCUMENTDB_URI,
-        { masterKey: process.env.DOCUMENTDB_KEY });
+module.exports.unlikeMovie = (movie) => {
+  return new Promise((resolve, reject) => {
+    const docdbClient = new DocumentDBClient(
+      process.env.DOCUMENTDB_URI,
+      { masterKey: process.env.DOCUMENTDB_KEY });
 
-      // const documentLink = `${collLink}/${movie.id}`;
-      const documentLink = movie._self;
-      const movieUpdate = Object.assign({}, movie, { liked: true });
+    const documentLink = movie._self;
+    const movieUpdate = Object.assign({}, movie, { liked: false });
 
-      docdbClient.replaceDocument(documentLink, movieUpdate, (err, updated) => {
-        if (err) {
-          reject(err);
-        }
-        else {
-          resolve(updated);
-        }
-      });
+    docdbClient.replaceDocument(documentLink, movieUpdate, (err, updated) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(updated);
+      }
     });
-  }
+  });
+}
 
-  function unlikeMovie(movie) {
-    return new Promise((resolve, reject) => {
-      const docdbClient = new DocumentDBClient(
-        process.env.DOCUMENTDB_URI,
-        { masterKey: process.env.DOCUMENTDB_KEY });
-
-      const documentLink = movie._self;
-      const movieUpdate = Object.assign({}, movie, { liked: false });
-
-      docdbClient.replaceDocument(documentLink, movieUpdate, (err, updated) => {
-        if (err) {
-          reject(err);
-        }
-        else {
-          resolve(updated);
-        }
-      });
-    });
-  }
-
-  return {
-    queryMovies,
-    likeMovie,
-    unlikeMovie
-  };
-})();
