09c0e29312e81ad589bd4da153ab2977bf6bd0cb
Thomas Stringer <tr.stringer@outlook.com>
2016-11-29 12:11:05 -0500

    add ability for movie component to retrieve poster url and set it

--- src/Movie.js
+++ src/Movie.js
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Movie.css';
 
-const Movie = (props) => {
-  return (
-    <div className="Movie">
-      <div>
-        {props.name}
+class Movie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posterUrl: ''
+    };
+  }
+
+  componentDidMount() {
+    console.log(`in componentDidMount for ${this.props.name}`);
+    const apiUrl = `http://netflixroulette.net/api/api.php?title=${encodeURIComponent(this.props.name)}`;
+    fetch(apiUrl)
+      .then(res => res.json())
+      .then(json => this.setState({posterUrl: json.poster}))
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="Movie">
+        <div>
+          {this.props.name}
+        </div>
+        <div>
+          <img
+            className="Movie-poster"
+            src={this.state.posterUrl} />
+        </div>
+        <div>
+          {this.props.liked ? "Love it!" : "Hate it"}
+        </div>
+        <div onClick={ this.props.likeMovie }>
+          <i className="fa fa-camera-retro"></i>
+        </div>
+        <div onClick={ this.props.unlikeMovie }>
+          <i className="fa fa-camera"></i>
+        </div>
       </div>
-      <div>
-        <img src="http://netflixroulette.net/api/posters/70098333.jpg" />
-      </div>
-      <div>
-        {props.liked ? "Love it!" : "Hate it"}
-      </div>
-      <div onClick={ props.likeMovie }>
-        <i className="fa fa-camera-retro"></i>
-      </div>
-      <div onClick={ props.unlikeMovie }>
-        <i className="fa fa-camera"></i>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Movie;
 
