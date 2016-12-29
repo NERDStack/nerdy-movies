4abf412d9dffb28f7742cd34980d9459cd3cc544
Thomas Stringer <tr.stringer@outlook.com>
2016-12-29 17:59:07 -0500

    (wip) add basic cleanup setup file

---
+++ setup.js
@@ -0,0 +1,10 @@
+module.exports = {
+  remove: [
+    { file: 'server/data/index.js' },
+    { file: 'server/data/config.js' }
+  ],
+  clean: [],
+  add: [
+    { file: 'server/data/.gitkeep' }
+  ]
+};
