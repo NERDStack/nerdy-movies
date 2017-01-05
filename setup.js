2bc454bf16dc4412a87761bfa5b4b797bbcbe013
Thomas Stringer <tr.stringer@outlook.com>
2017-01-05 07:01:04 -0500

    (wip) create initial draft of cleanup direction

--- setup.js
+++ setup.js
@@ -1,10 +1,40 @@
 module.exports = {
   remove: [
     { file: 'server/data/index.js' },
-    { file: 'server/data/config.js' }
+    { file: 'server/data/config.js' },
+    { file: 'src/actions/action-types.js' },
+    { file: 'src/actions/movie-actions.js' },
+    { file: 'src/reducers/index.js' },
+    { file: 'src/reducers/movies-reducer.js' },
+    { file: 'src/Movie.css' },
+    { file: 'src/Movie.js' },
+    { file: 'src/MoviesContainer.css' },
+    { file: 'src/MoviesContainer.js' },
+    { file: 'src/MoviesList.css' },
+    { file: 'src/MoviesList.js' },
+    { file: 'setup.js' }
   ],
-  clean: [],
+  clean: [
+    {
+      file: 'server/routes/api.js',
+      deleteRange: '4,54'
+    },
+    {
+      file: 'src/App.js',
+      pattern: /Movies/
+    },
+    {
+      file: 'README.md',
+      clear: true
+    },
+    {
+      file: 'package.json',
+      pattern: /cleanup.*mop/
+    }
+  ],
   add: [
-    { file: 'server/data/.gitkeep' }
+    { file: 'server/data/.gitkeep' },
+    { file: 'src/actions/.gitkeep' },
+    { file: 'src/reducers/.gitkeep' }
   ]
 };
