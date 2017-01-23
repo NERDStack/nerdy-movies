a263c718a13ac675f5ec8c72514aaa083acebf47
Thomas Stringer <tr.stringer@outlook.com>
2017-01-23 14:11:45 -0500

    add local documentdb run script as a separate file

--- scripts/start-local-documentdb.ps1
+++ scripts/start-local-documentdb.ps1
@@ -2,6 +2,7 @@
 # CreateEnvVars.ps1
 #
 
+# Set to use local emulator -- default uri/key
 $documentdb_uri = "https://localhost:8081"
 $documentdb_key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
 
